d534f8c018d7cb9c48547188ac666010be64c9f3
Théo armengou <theo.armengou@epitech.eu>
2020-07-28 03:03:00 +0200

    Add force refresh button, settings button on overlay, overlay sizes and setting for refresh interval

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 // Vendor
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
 const settings = require('electron-settings');
 
 // Constants
 const DEVTOOL_OPTIONS = {mode: 'detach'};
+
+const OVERLAY_SIZE = [
+  {
+    height: 70,
+    width: 460
+  },
+  {
+    height: 60,
+    width: 400
+  },
+  {
+    height: 50,
+    width: 350
+  }
+];
 
 let overlayWindow;
 let settingsWindow;
@@ -19,8 +34,14 @@ function createOverlayWindow() {
     resizable: false,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
   };
+
+  if (settings.has('overlay.size')) {
+    const {height, width} = OVERLAY_SIZE[settings.get('overlay.size')];
+    windowOptions.height = height;
+    windowOptions.width = width;
+  }
 
   if (settings.has('position')) {
     windowOptions.x = settings.get('position.x');
@@ -60,7 +81,10 @@ function createSettingsWindow() {
 
   settingsWindow.setMenu(null);
 
-  settingsWindow.on('closed', () => overlayWindow.send('forceChaosRecipeRefresh'));
+  settingsWindow.on('closed', () => {
+    overlayWindow.send('forceChaosRecipeRefresh')
+    settingsWindow = null;
+  });
 
   if (debug) settingsWindow.webContents.openDevTools(DEVTOOL_OPTIONS);
 }
@@ -71,3 +95,15 @@ app.on('ready', () => {
 });
 
 app.on('window-all-closed', () => app.quit());
+
+ipcMain.on('overlay-size-changed', () => {
+  const {height, width} = OVERLAY_SIZE[settings.get('overlay.size')];
+  overlayWindow.setBounds({width, height});
+});
+
+ipcMain.on('open-options', () => {
+  if (!settingsWindow) {
+    createSettingsWindow();
+    settingsWindow.show();
+  }
+});
